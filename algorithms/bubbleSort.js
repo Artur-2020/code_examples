c23024422fde3f23dcb0f09f935c85ad7995627b
Arthur Sahakyan <artursahakyan262@gmail.com>
2025-02-01 00:27:18 +0400

    feat(algorithms): added bubbleSort algorithm

---
+++ algorithms/bubbleSort.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr) {
+    for (let i = 0, len = arr.length; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([64, 25, 12, 22, 11]));
